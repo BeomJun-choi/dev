bece211e58e1b18966e49ce11578939148fd6108
BeomJunChoi <woor019@gmail.com>
2020-07-28 23:16:55 +0900

    vuetify

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <baseline></baseline>
+</template>
+
+<script>
+import baseline from '@/layouts/baseline.vue';
+
+
+export default {
+  name: 'App',
+
+  components: {
+    baseline,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
